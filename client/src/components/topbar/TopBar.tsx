c3b31ceddc02a5810104d915bef3985b7cfb406c
epsxy <epsxy@users.noreply.github.com>
2020-11-05 01:03:38 +0100

    refactor(client): update topbar style and links

--- client/src/components/topbar/TopBar.tsx
+++ client/src/components/topbar/TopBar.tsx
@@ -12,25 +12,27 @@ const Root = styled.div`
     align-items: center;
     height: 50px;
     margin-right: 1em;
-`
+`;
 
 const TopBarElement = styled.a`
-    margin: 0em .5em;
+    margin: 0em 0.5em;
     color: white;
     text-decoration: none;
     &:hover {
-        color: #FFE440;
+        color: #ffe440;
         text-shadow: 5px 5px rebeccapurple;
     }
-`
+`;
 
 function TopBar() {
     return (
         <Root>
-            <TopBarElement href={`http://${window.location.hostname}:${window.location.port}`}>Message viewer</TopBarElement>
-            <TopBarElement href="http://github.com/epsxy.com">Github</TopBarElement>
+            <TopBarElement href={`${window.location.protocol}//${window.location.hostname}:${window.location.port}`}>
+                Message viewer
+            </TopBarElement>
+            <TopBarElement href="https://github.com/epsxy/first-rails-api">Github</TopBarElement>
         </Root>
-    )
+    );
 }
 
 export default TopBar;
